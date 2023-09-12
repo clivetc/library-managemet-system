970e64fc59bcabcc4f8dee4f0c66b2c33d3200d4
clivetc <clivetchikwape@gmail.com>
2023-09-12 21:15:49 +0300

    fixes

--- utils/db.ts
+++ utils/db.ts
@@ -1,11 +1,17 @@
-import { db } from "@vercel/postgres";
+import { Client } from "pg"; // Import the Client class from 'pg'
 
 async function connectToDatabase() {
+  const client = new Client({
+    connectionString: process.env.POSTGRES_URL, // Use your PostgreSQL connection URL from environment variables
+  });
+
   try {
-    await db.connect();
+    await client.connect(); // Connect to the database
     console.log("Connected to the database");
   } catch (error) {
     console.error("Error connecting to the database:", error);
+  } finally {
+    client.end(); // Close the database connection when done
   }
 }
 
