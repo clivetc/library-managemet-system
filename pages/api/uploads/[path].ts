ebe44681161021d122cb3eb7c47b2a75f239ca3f
clivetc <clivetchikwape@gmail.com>
2024-01-25 22:26:22 +0200

    quick fix

--- pages/api/uploads/[path].ts
+++ pages/api/uploads/[path].ts
@@ -1,7 +1,5 @@
 import { join } from "path";
-import { createReadStream } from "fs";
 import { cors } from "@/utils/middleware";
-import { get } from "@vercel/blob";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,8 +8,25 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const { path } = req.query;
 
 	if (typeof path === "string") {
-		const { stream } = await get(`uploads/${path}`);
-		stream.pipe(res);
+		const response = await fetch(`/uploads/${path}`);
+		if (response.ok) {
+			const reader = response.body!.getReader();
+
+			try {
+				while (true) {
+					const { done, value } = await reader.read();
+					if (done) break;
+					if (value) res.write(Buffer.from(value));
+				}
+
+				res.end();
+			} catch (error) {
+				console.error("Error piping streams:", error);
+				res.status(500).json({ error: "Internal Server Error" });
+			}
+		} else {
+			res.status(404).json({ error: "Image not found" });
+		}
 	} else {
 		res.status(400).json({ error: "Invalid image path" });
 	}
